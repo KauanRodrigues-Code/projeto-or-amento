9698ae1d6e5b10644420c86faebd4a0a446863bb
KauanRodrigues-Code <kauancorinthiano@icloud.com>
2026-02-12 13:49:04 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// Reveal padrão
-gsap.utils.toArray(".reveal").forEach((el) => {
-  gsap.to(el, {
-    scrollTrigger: {
-      trigger: el,
-      start: "top 85%",
+/* Scroll Reveal */
+gsap.utils.toArray(".reveal").forEach((el)=>{
+  gsap.to(el,{
+    scrollTrigger:{
+      trigger:el,
+      start:"top 85%"
     },
-    y: 0,
-    opacity: 1,
-    duration: 1.2,
-    ease: "power3.out"
+    y:0,
+    opacity:1,
+    duration:1.2,
+    ease:"power3.out"
   });
 });
 
-// Parallax + zoom + rotação nas imagens
-gsap.utils.toArray(".watch").forEach((img) => {
-  gsap.to(img, {
-    scrollTrigger: {
-      trigger: img,
-      start: "top bottom",
-      end: "bottom top",
-      scrub: true
+/* Parallax nas imagens */
+gsap.utils.toArray(".watch").forEach((img)=>{
+  gsap.to(img,{
+    scrollTrigger:{
+      trigger:img,
+      start:"top bottom",
+      end:"bottom top",
+      scrub:true
     },
-    y: -120,
-    scale: 1.1,
-    rotate: 5
+    y:-120,
+    scale:1.1,
+    rotate:5
   });
 });
+
+/* Counter Animation */
+const counters=document.querySelectorAll(".counter");
+
+counters.forEach(counter=>{
+  ScrollTrigger.create({
+    trigger:counter,
+    start:"top 85%",
+    onEnter:()=>animateCounter(counter)
+  });
+});
+
+function animateCounter(counter){
+  const target=parseFloat(counter.getAttribute("data-target"));
+  let count=0;
+  const increment=target/60;
+
+  function update(){
+    count+=increment;
+    if(count<target){
+      counter.innerText=count.toFixed(1);
+      requestAnimationFrame(update);
+    }else{
+      counter.innerText=target;
+    }
+  }
+  update();
+}
